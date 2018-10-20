95d03c84e78492cb402cd653f71f52767eb6fc0f
Lawrence Cushman <lacush@microsoft.com>
2018-10-19 20:19:29 -0700

    add super secure keys. Please don't steal them.

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,7 +1,7 @@
 function submitComment(commentControl) {
     var comments = document.getElementsByName(commentControl)[0].value;
-    var subscriptionKey = "TEXT_ANALYTICS_API_KEY";
-    var url = "TEXT_ANALYTICS_ENDPOINT/sentiment";
+    var subscriptionKey = "ed112e14ca454d0591a1c57a8a6ed506";
+    var url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
 
     var payload = '{ "documents": [ { "language": "en-US", "id": "1", "text": "' + comments + '" }]}';
 
